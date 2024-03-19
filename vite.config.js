7682cffcf7811560e1b1e2bd0b50259f92805cc7
okaozman-create <okanozman88@gmail.com>
2024-03-20 02:16:56 +0300

    test for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   root: "src",
   build: {
     outDir: "../build",
     emptyOutDir: true,
+    rollupOptions: {
+      external: ['aws-amplify'], 
+    }
   },
   plugins: [
     svgr({ svgrOptions: { ref: true } }),
@@ -19,4 +22,33 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      
+    },
+  },
 });
+
+
+// import { defineConfig } from "vite";
+// import svgr from "vite-plugin-svgr";
+// import react from "@vitejs/plugin-react";
+// import { VitePWA } from "vite-plugin-pwa";
+
+// export default defineConfig({
+//   root: "src",
+//   build: {
+//     outDir: "../build",
+//     emptyOutDir: true,
+//   },
+//   plugins: [
+//     svgr({ svgrOptions: { ref: true } }),
+//     react(),
+//     VitePWA({
+//       workbox: {
+//         clientsClaim: true,
+//         skipWaiting: true,
+//       },
+//     }),
+//   ],
+// });
