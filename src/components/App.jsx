0d92d4a3da8b785086f9ff2d20ed30e70753b415
okaozman-create <okanozman88@gmail.com>
2024-03-20 02:57:57 +0300

    test for deployment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ addRouteComponent("Home", Home);
 // addRouteComponent("AuthFlow", AuthFlow);
 
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports.js';
+import config from '../aws-exports.js';
 
 Amplify.configure(config);
 
